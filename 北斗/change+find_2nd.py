714702d805756fbd3bd1fcd4c1654226b79380f4
Zzw <48195029+GitZzw@users.noreply.github.com>
2020-11-01 21:59:00 +0800

    Add files via upload

--- 北斗/change+find_2nd.py
+++ 北斗/change+find_2nd.py
@@ -25,6 +25,7 @@ from time import time
 import numpy as np
 import pyrealsense2 as rs
 import cv2
+import socket
 from openvino.inference_engine import IENetwork, IECore
 
 logging.basicConfig(format="[ %(levelname)s ] %(message)s", level=logging.INFO, stream=sys.stdout)
@@ -179,7 +180,7 @@ def find_min_box(obj_list_,color_frame_,depth_frame_):
     global plane_x,plane_y,plane_z
     box_min = 100000000
     obj_min = obj_list_[0]
-    #find min box
+    #find min obj
     for obj_ in obj_list_:
         box_area = abs((obj_['xmax'] - obj_['xmin'])*(obj_['ymax'] - obj_['ymin']))
         if box_area < box_min:
@@ -193,30 +194,39 @@ def find_min_box(obj_list_,color_frame_,depth_frame_):
     crop_img = color_frame_.copy()
 
     cv2.rectangle(color_frame_, (obj_min['xmin'], obj_min['ymin']), (obj_min['xmax'], obj_min['ymax']), color=(100,150,200), thickness=2)
-
+    # length and width min & max
     w_min = obj_min['xmin']  if obj_min['xmin'] > 0 else 0
     w_max = obj_min['xmax'] if obj_min['xmax'] < 640 else 640
     h_min = obj_min['ymin']  if obj_min['ymin']  > 0 else 0
     h_max = obj_min['ymax']  if obj_min['ymax']  < 480 else 480
-    #crop_img = crop_img[obj['xmin']:obj['xmax']+100, obj['ymin']:obj['ymax']+100]
 
     crop_img = crop_img[h_min:h_max, w_min:w_max] # first height, second width
-
+    # length and width
     depth_box_width=obj_min['xmax']-obj_min['xmin']
     depth_box_height=obj_min['ymax']-obj_min['ymin']
+
+
+    # delta_rate?
+    # length and width min & max compressed
     delta_rate=0.3
     x_box_min=int(obj_min['xmin']+depth_box_width*delta_rate)
     y_box_min=int(obj_min['ymin']+depth_box_height*delta_rate)
     x_box_max=int(obj_min['xmax']-depth_box_width*delta_rate)
     y_box_max=int(obj_min['ymax']-depth_box_height*delta_rate)
-    #print(x_box_min)
+
+
+    # length and with compressed
     after_width=(depth_box_width*(1-2*delta_rate))
     after_height=(depth_box_height*(1-2*delta_rate))
     depth_crop_img_=depth_frame_[y_box_min:y_box_max,x_box_min:x_box_max]
     z_pos=depth_crop_img_.sum()/(after_width*after_height)*0.001
+
     #z_pos=sum(map(sum,bb))/(after_width*after_height)*0.001
+
     x_pos = (0.5 * (x_box_min + x_box_max) - cx) * z_pos / fx
+
     y_pos = (0.5 * (y_box_min + y_box_max) - cy) * z_pos / fy
+
     print('0.5 * (y_box_min + y_box_max):',0.5 * (y_box_min + y_box_max))
     print('(0.5 * (y_box_min + y_box_max) - cy):',(0.5 * (y_box_min + y_box_max) - cy))
     print('z_pos:',z_pos)
@@ -234,7 +244,6 @@ def find_min_box(obj_list_,color_frame_,depth_frame_):
 
 def main():
     #args = build_argparser().parse_args()
-
     #model_xml = args.model
     #model_bin = os.path.splitext(model_xml)[0] + ".bin"
     global fx, fy, cx, cy, plane_x, plane_y, plane_z
@@ -245,7 +254,7 @@ def main():
     args.bin = 'frozen_darknet_yolov3_model.bin'
     args.device = 'MYRIAD'
     args.labels = 'frozen_darknet_yolov3_model.mapping'
-    args.input = 'outdoorsd435.avi'
+    args.input = '3.mp4'
     args.prob_threshold = 0.6
     args.iou_threshold = 0.6
     args.raw_output_message = True
@@ -254,6 +263,7 @@ def main():
 
     #TCP IP SETUP
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     server.bind(('127.0.0.1',8000))
     server.listen(5)
     print("waiting msg ...")
@@ -337,174 +347,175 @@ def main():
     log.info("Starting inference...")
     print("To close the application, press 'CTRL+C' here or switch to the output window and press ESC key")
     print("To switch between sync/async modes, press TAB key in the output window")
-    while True:
-        # Here is the first asynchronous point: in the Async mode, we capture frame to populate the NEXT infer request
-        # in the regular mode, we capture frame to the CURRENT infer request
-        if is_async_mode:
-            next_frame = pipeline.wait_for_frames()
-            align_to_depth.process(next_frame)
-            get_next_frame = next_frame.get_color_frame()
-            get_next_depth_frame = next_frame.get_depth_frame()
+    try:
+        while True:
+            # Here is the first asynchronous point: in the Async mode, we capture frame to populate the NEXT infer request
+            # in the regular mode, we capture frame to the CURRENT infer request
+            if is_async_mode:
+                next_frame = pipeline.wait_for_frames()
+                align_to_depth.process(next_frame)
+                get_next_frame = next_frame.get_color_frame()
+                get_next_depth_frame = next_frame.get_depth_frame()
 
-            next_depth_frame = np.asanyarray(get_next_depth_frame.get_data())
-            next_frame = np.asanyarray(get_next_frame.get_data())
+                next_depth_frame = np.asanyarray(get_next_depth_frame.get_data())
+                next_frame = np.asanyarray(get_next_frame.get_data())
 
-        else:
-            frame = pipeline.wait_for_frames()
+            else:
+                frame = pipeline.wait_for_frames()
 
-            # 对齐时间戳
-            align_to_depth = rs.align(rs.stream.depth)
-            align_to_depth.process(frame)
-            #深度和rgb
-            get_next_frame = frame.get_color_frame()
-            get_depth_frame = frame.get_depth_frame()
-            depth_frame = np.asanyarray(get_depth_frame.get_data())
-            frame = np.asanyarray(get_next_frame.get_data())
+                # 对齐时间戳
+                align_to_depth = rs.align(rs.stream.depth)
+                align_to_depth.process(frame)
+                #深度和rgb
+                get_next_frame = frame.get_color_frame()
+                get_depth_frame = frame.get_depth_frame()
+                depth_frame = np.asanyarray(get_depth_frame.get_data())
+                frame = np.asanyarray(get_next_frame.get_data())
 
-        if not get_next_frame:
-            continue
+            if not get_next_frame:
+                continue
 
 
-        print("next_frame")
-        if is_async_mode:
-            request_id = next_request_id
-            in_frame = cv2.resize(next_frame, (w, h))
-        else:
-            request_id = cur_request_id
-            in_frame = cv2.resize(frame, (w, h))
+            print("next_frame")
+            if is_async_mode:
+                request_id = next_request_id
+                in_frame = cv2.resize(next_frame, (w, h))
+            else:
+                request_id = cur_request_id
+                in_frame = cv2.resize(frame, (w, h))
 
-        # resize input_frame to network size
-        in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-        in_frame = in_frame.reshape((n, c, h, w))
+            # resize input_frame to network size
+            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+            in_frame = in_frame.reshape((n, c, h, w))
 
-        # Start inference
-        start_time = time()
-        exec_net.start_async(request_id=request_id, inputs={input_blob: in_frame})
-        det_time = time() - start_time
+            # Start inference
+            start_time = time()
+            exec_net.start_async(request_id=request_id, inputs={input_blob: in_frame})
+            det_time = time() - start_time
 
-        # Collecting object detection results
-        objects = list()
-        if exec_net.requests[cur_request_id].wait(-1) == 0:
-            output = exec_net.requests[cur_request_id].outputs
+            # Collecting object detection results
+            objects = list()
+            if exec_net.requests[cur_request_id].wait(-1) == 0:
+                output = exec_net.requests[cur_request_id].outputs
+
+                start_time = time()
+                for layer_name, out_blob in output.items():
+                    out_blob = out_blob.reshape(net.layers[net.layers[layer_name].parents[0]].shape)
+                    layer_params = YoloParams(net.layers[layer_name].params, out_blob.shape[2])
+                    #log.info("Layer {} parameters: ".format(layer_name))
+                    #layer_params.log_params()
+                    objects += parse_yolo_region(out_blob, in_frame.shape[2:],
+                                                 frame.shape[:-1], layer_params,
+                                                 args.prob_threshold)
+                parsing_time = time() - start_time
+
+            # Filtering overlapping boxes with respect to the --iou_threshold CLI parameter
+            objects = sorted(objects, key=lambda obj : obj['confidence'], reverse=True)
+            for i in range(len(objects)):
+                if objects[i]['confidence'] == 0:
+                    continue
+                for j in range(i + 1, len(objects)):
+                    if intersection_over_union(objects[i], objects[j]) > args.iou_threshold:
+                        objects[j]['confidence'] = 0
+
+            # Drawing objects with respect to the --prob_threshold CLI parameter
+            objects = [obj for obj in objects if obj['confidence'] >= args.prob_threshold]
+
+            if len(objects) and args.raw_output_message:
+                log.info("\nDetected boxes for batch {}:".format(1))
+                log.info(" Class ID | Confidence | XMIN | YMIN | XMAX | YMAX | COLOR ")
+
+            origin_im_size = frame.shape[:-1]
+
+            #存储识别到的obj
+            obj_list = list()
+
+            plane_x = 10
+            plane_y = 0
+            plane_z = 0
+
+            for obj in objects:
+                # Validation bbox of detected object
+                if obj['xmax'] > origin_im_size[1] or obj['ymax'] > origin_im_size[0] or obj['xmin'] < 0 or obj['ymin'] < 0:
+                    continue
+                color = (255,255,0)
+                det_label = labels_map[obj['class_id']] if labels_map and len(labels_map) >= obj['class_id'] else \
+                    str(obj['class_id'])
+
+                if args.raw_output_message:
+                    log.info(
+                        "{:^9} | {:10f} | {:4} | {:4} | {:4} | {:4} | {} ".format(det_label, obj['confidence'], obj['xmin'],
+                                                                                  obj['ymin'], obj['xmax'], obj['ymax'],
+                                                                                  color))
+
+                #cv2.rectangle(frame, (obj['xmin'], obj['ymin']), (obj['xmax'], obj['ymax']), color, 2)
+                # cv2.putText(frame,
+                #             "#" + det_label + ' ' + str(round(obj['confidence'] * 100, 1)) + ' %',
+                #             (obj['xmin'], obj['ymin'] - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
+
+                # #添加obj
+                obj_list.append(obj)
+
+
+            if len(obj_list) > 0:
+                objmin = find_min_box(obj_list,frame,depth_frame)
+                cv2.putText(frame,
+                        "#" + det_label + ' ' + str(round(objmin['confidence'] * 100, 1)) + ' %',
+                        (objmin['xmin'], objmin['ymin'] - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
+            # TCP/IP
+            #TCP/IP 1 receive
+            print("receive msg ...")
+            data = conn.recv(5).decode('utf-8')
+
+
+            if len(data)==5:
+                print("now send msg ......")
+                send_data = [int(plane_x*1000), int(plane_y*1000), int(plane_z*1000)]
+                print(send_data)
+                send_data_byte = bytes(0)
+                for i in range(len(send_data)):
+                    senddata1000 = str(send_data[i])+','
+                    send_data_byte += senddata1000.encode()
+                print(send_data_byte)
+                conn.send(send_data_byte)
+
+
+
+            # Draw performance stats over frame
+            inf_time_message = "Inference time: N\A for async mode" if is_async_mode else \
+                "Inference time: {:.3f} ms".format(det_time * 1e3)
+            render_time_message = "OpenCV rendering time: {:.3f} ms".format(render_time * 1e3)
+            async_mode_message = "Async mode is on. Processing request {}".format(cur_request_id) if is_async_mode else \
+                "Async mode is off. Processing request {}".format(cur_request_id)
+            parsing_message = "YOLO parsing time is {:.3f}".format(parsing_time * 1e3)
+
+            cv2.putText(frame, inf_time_message, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
+            cv2.putText(frame, render_time_message, (15, 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (10, 10, 200), 1)
+            # cv2.putText(frame, async_mode_message, (10, int(origin_im_size[0] - 20)), cv2.FONT_HERSHEY_COMPLEX, 0.5,
+            #             (10, 10, 200), 1)
+            cv2.putText(frame, parsing_message, (15, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (10, 10, 200), 1)
 
             start_time = time()
-            for layer_name, out_blob in output.items():
-                out_blob = out_blob.reshape(net.layers[net.layers[layer_name].parents[0]].shape)
-                layer_params = YoloParams(net.layers[layer_name].params, out_blob.shape[2])
-                log.info("Layer {} parameters: ".format(layer_name))
-                layer_params.log_params()
-                objects += parse_yolo_region(out_blob, in_frame.shape[2:],
-                                             frame.shape[:-1], layer_params,
-                                             args.prob_threshold)
-            parsing_time = time() - start_time
+            cv2.imshow("DetectionResults", frame)
+            render_time = time() - start_time
 
-        # Filtering overlapping boxes with respect to the --iou_threshold CLI parameter
-        objects = sorted(objects, key=lambda obj : obj['confidence'], reverse=True)
-        for i in range(len(objects)):
-            if objects[i]['confidence'] == 0:
-                continue
-            for j in range(i + 1, len(objects)):
-                if intersection_over_union(objects[i], objects[j]) > args.iou_threshold:
-                    objects[j]['confidence'] = 0
+            if is_async_mode:
+                cur_request_id, next_request_id = next_request_id, cur_request_id
+                frame = next_frame
 
-        # Drawing objects with respect to the --prob_threshold CLI parameter
-        objects = [obj for obj in objects if obj['confidence'] >= args.prob_threshold]
-
-        if len(objects) and args.raw_output_message:
-            log.info("\nDetected boxes for batch {}:".format(1))
-            log.info(" Class ID | Confidence | XMIN | YMIN | XMAX | YMAX | COLOR ")
-
-        origin_im_size = frame.shape[:-1]
-
-        #存储识别到的obj
-        obj_list = list()
+            key = cv2.waitKey(wait_key_code)
 
 
-        for obj in objects:
-            # Validation bbox of detected object
-            if obj['xmax'] > origin_im_size[1] or obj['ymax'] > origin_im_size[0] or obj['xmin'] < 0 or obj['ymin'] < 0:
-                continue
-            color = (255,255,0)
-            det_label = labels_map[obj['class_id']] if labels_map and len(labels_map) >= obj['class_id'] else \
-                str(obj['class_id'])
-
-            if args.raw_output_message:
-                log.info(
-                    "{:^9} | {:10f} | {:4} | {:4} | {:4} | {:4} | {} ".format(det_label, obj['confidence'], obj['xmin'],
-                                                                              obj['ymin'], obj['xmax'], obj['ymax'],
-                                                                              color))
-
-            #cv2.rectangle(frame, (obj['xmin'], obj['ymin']), (obj['xmax'], obj['ymax']), color, 2)
-            # cv2.putText(frame,
-            #             "#" + det_label + ' ' + str(round(obj['confidence'] * 100, 1)) + ' %',
-            #             (obj['xmin'], obj['ymin'] - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
-
-            # #添加obj
-            obj_list.append(obj)
-
-
-        if len(obj_list) > 0:
-            objmin = find_min_box(obj_list,color_frame,depth_frame)
-            cv2.putText(color_frame,
-                    "#" + det_label + ' ' + str(round(objmin['confidence'] * 100, 1)) + ' %',
-                    (objmin['xmin'], objmin['ymin'] - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
-        # TCP/IP
-        #TCP/IP 1 receive
-        print("receive msg ...")
-        data = conn.recv(5).decode('utf-8')
-
-
-        plane_x = 10
-        plane_y = 0
-        plane_z = 0
-        if len(data)==5:
-            print("now send msg ......")
-            send_data = [int(plane_x*1000), int(plane_y*1000), int(plane_z*1000)]
-            print(send_data)
-            send_data_byte = bytes(0)
-            for i in range(len(send_data)):
-                print(send_data[i])
-                senddata1000 = str(send_data[i])+','
-                print(senddata1000.encode())
-                send_data_byte += senddata1000.encode()
-            conn.send(send_data_byte)
-
-
-
-        # Draw performance stats over frame
-        inf_time_message = "Inference time: N\A for async mode" if is_async_mode else \
-            "Inference time: {:.3f} ms".format(det_time * 1e3)
-        render_time_message = "OpenCV rendering time: {:.3f} ms".format(render_time * 1e3)
-        async_mode_message = "Async mode is on. Processing request {}".format(cur_request_id) if is_async_mode else \
-            "Async mode is off. Processing request {}".format(cur_request_id)
-        parsing_message = "YOLO parsing time is {:.3f}".format(parsing_time * 1e3)
-
-        cv2.putText(frame, inf_time_message, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
-        cv2.putText(frame, render_time_message, (15, 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (10, 10, 200), 1)
-        # cv2.putText(frame, async_mode_message, (10, int(origin_im_size[0] - 20)), cv2.FONT_HERSHEY_COMPLEX, 0.5,
-        #             (10, 10, 200), 1)
-        cv2.putText(frame, parsing_message, (15, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (10, 10, 200), 1)
-
-        start_time = time()
-        cv2.imshow("DetectionResults", frame)
-        render_time = time() - start_time
-
-        if is_async_mode:
-            cur_request_id, next_request_id = next_request_id, cur_request_id
-            frame = next_frame
-
-        key = cv2.waitKey(wait_key_code)
-
-
-        # ESC key
-        if key == 27:
-            break
-        # Tab key
-        if key == 9:
-            exec_net.requests[cur_request_id].wait()
-            is_async_mode = not is_async_mode
-            log.info("Switched to {} mode".format("async" if is_async_mode else "sync"))
-
+            # ESC key
+            if key == 27:
+                break
+            # Tab key
+            if key == 9:
+                exec_net.requests[cur_request_id].wait()
+                is_async_mode = not is_async_mode
+                log.info("Switched to {} mode".format("async" if is_async_mode else "sync"))
+    finally:
+        pipeline.stop()
     cv2.destroyAllWindows()
 
 
